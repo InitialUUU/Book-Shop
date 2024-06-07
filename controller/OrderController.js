2610ec480175a6ca582efc33818f721b335ed325
InitialUUU <124362567+InitialUUU@users.noreply.github.com>
2024-06-07 18:18:31 +0900

    240530

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,31 +1,33 @@
 // OrderController.js
 
-const conn = require('../mariadb'); // db 모듈
+// const conn = require('../mariadb'); // db 모듈
+const mariadb = require('mysql2/promise');
 const {StatusCodes} = require('http-status-codes'); // status-code 모듈
 
-const order = (req,res) => {
+const order = async (req,res) => {
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dataStrings: true
+    });
+
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    let delivery_id = 3;
-    let order_id = 2;
+    let delivery_id;
+    let order_id;
 
     let sql = "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)";
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return res.status(StatusCodes.BAD_REQUEST).end();
-    //         }
+    
+    let [results] = await conn.query(sql, values);
 
-    //         delivery_id = results.insertId; // postman의 results에서 자동으로 반환해준 값
+    console.log(results);
 
-    //         return res.status(StatusCodes.OK).json(results);
-    // })
-
-    sql = `INSERT INTO orders(book_title, total_quantity, total_price, user_id, delivery_id)
-            VALUES (?, ?, ?, ?, ?)`;
-    values = [ firstBookTitle, totalQuantity, totalPrice, userId, delivery_id]
+    // sql = `INSERT INTO orders(book_title, total_quantity, total_price, user_id, delivery_id)
+    //         VALUES (?, ?, ?, ?, ?)`;
+    // values = [ firstBookTitle, totalQuantity, totalPrice, userId, delivery_id]
     // conn.query(sql, values,
     //     (err, results) => {
     //         if (err) {
@@ -34,32 +36,25 @@ const order = (req,res) => {
     //         }
 
     //         order_id = results.insertId; // postman의 results에서 자동으로 반환해준 값
-    //         console.log(order_id);
-
-    //         return res.status(StatusCodes.OK).json(results);
     // })
 
-    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+//     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
 
-    // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
-    values = [];
-    items.forEach((item) => {
-        values.push([order_id, item.book_id, item.quantity]);
-        console.log(values);
-    })
-    conn.query(sql, [values],
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
+//     // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
+//     values = [];
+//     items.forEach((item) => {
+//         values.push([order_id, item.book_id, item.quantity]);
+//     })
+//     conn.query(sql, [values],
+//         (err, results) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(StatusCodes.BAD_REQUEST).end();
+//             }
 
-            order_id = results.insertId; // postman의 results에서 자동으로 반환해준 값
-            console.log(order_id);
-
-            return res.status(StatusCodes.OK).json(results);
-    })
-};
+//             return res.status(StatusCodes.OK).json(results);
+//     })
+// };
 
 const getOrders = (req,res) => {
     res.json('주문 목록 조회');
--- mariadb.js
+++ mariadb.js
@@ -1,15 +1,19 @@
 // mariadb.js
 
 // mysql 모듈 소환
-const mariadb = require('mysql2');
+const mariadb = require('mysql2/promise');
 
 // DB와 연결 통로 생성
-const connection = mariadb.createConnection({
-    host : '127.0.0.1',
-    user : 'root',
-    password : 'root',
-    database : 'Bookshop',
-    dataStrings : true
-});
+const connection = async () => {
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dataStrings: true
+    });
 
-module.exports = connection
+    return conn;
+}
+
+module.exports = connection;
